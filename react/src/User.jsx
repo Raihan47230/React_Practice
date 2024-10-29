f535b4076dc19124ca355b70ff4e30a6777d45a3
Raihan R <rr5194799@gmail.com>
2024-10-30 02:57:32 +0600

    Update User.jsx

--- react/src/User.jsx
+++ react/src/User.jsx
@@ -3,7 +3,7 @@ export default function User({TotalData}) {
      const{id,title}=TotalData
   return (
     <div className="boxer">
-     <h4>title : {title}</h4>
+      <h4>title : {title}</h4>
      <h4>id : {id}</h4>
     </div>
   )
